b951ae06f3b58aa8f5ecbd368fab9c036df0f6ee
Hari <hari20115002@gmail.com>
2026-09-22 16:37:54 +0530

    updated

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello DevOps");
 	System.out.println("Learning Git");
+	System.out.println("Add some change");
     }
 }
